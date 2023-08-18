7de58679bdb6f81cf1e770bffbe5446164796a5f
kai <sea.k.naka@gmail.com>
2023-08-19 05:38:20 +0900

    コンポーネントの使い方を知る

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"))
 root.render(<App />)
