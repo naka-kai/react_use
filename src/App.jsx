7f5d5f129deb173e5820ff0d7389eedf080c9bb3
kai <sea.k.naka@gmail.com>
2023-08-19 07:53:27 +0900

    Propsを知る

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+
+  const contentLedyStyle = {
+    color: "pink",
+    fontSize: "18px",
+  };
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">
+        元気です！
+      </ColorfulMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+}
+
+export default App;
